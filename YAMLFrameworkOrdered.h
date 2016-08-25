b1439d34d6e44d48c3a3ac6e2d08762fcbd10674
CodeEagle <stasura@hotmail.com>
2016-08-25 14:56:25 +0800

    :octocat: carthage support

---
+++ YAMLFrameworkOrdered.h
@@ -0,0 +1,17 @@
+//
+//  YAMLFrameworkOrdered.h
+//  YAML
+//
+//  Created by LawLincoln on 16/8/25.
+//
+//
+#import <Foundation/Foundation.h>
+//! Project version number for YAMLFrameworkOrdered.
+FOUNDATION_EXPORT double YAMLFrameworkOrderedVersionNumber;
+
+//! Project version string for YAMLFrameworkOrdered.
+FOUNDATION_EXPORT const unsigned char YAMLFrameworkOrderedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Handlebars/PublicHeader.h>
+#import <YAMLFrameworkOrdered/YAMLSerialization.h>
+#import <YAMLFrameworkOrdered/M13OrderedDictionary.h>
